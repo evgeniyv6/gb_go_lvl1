3156901b3d1a50a71d9a89009c5d12fb5c44208c
evgeniy_vakhrushev <evgeniyv6@gmail.com>
2020-12-09 22:28:00 +0300

    add exclamation point

--- 01-go-level-1/slices/main.go
+++ 01-go-level-1/slices/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from slices dir")
+	fmt.Println("Hello from slices dir! Add exclamation mark.")
 	xsOrign := []int{9, 9, 8, 8, 7, 7, 6, 5, 4, 3, 2, 1}
 	fmt.Printf("Origin slice - %d", xsOrign)
 	slactn.ReverseSlice(xsOrign)
