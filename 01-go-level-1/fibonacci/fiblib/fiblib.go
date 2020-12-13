0c7ba90cf6a092618a3ce7fb8552e433d683aca8
evgeniy_vakhrushev <evgeniyv6@gmail.com>
2020-12-13 09:04:53 +0300

    add fibonacci algos: golden ration, anonymous func and using map

--- 01-go-level-1/fibonacci/fiblib/fiblib.go
+++ 01-go-level-1/fibonacci/fiblib/fiblib.go
@@ -1,8 +1,52 @@
 package fiblib
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// FibonacciMapUse desc
-func FibonacciMapUse() {
-	fmt.Println("Map using")
+// FibonacciMapUse descr (bif o: O(n))
+func FibonacciMapUse(n int) (fibMap map[int]int) {
+	fmt.Println("--== Fibonacci map using ==--")
+	fibMap = make(map[int]int)
+	if n < 0 {
+		return
+	}
+	var num int
+	for i := 0; i <= n; i++ {
+		if i <= 1 {
+			num = i
+		} else {
+			num = fibMap[i-1] + fibMap[i-2]
+		}
+		fibMap[i] = num
+	}
+	return
+}
+
+// FibonacciAnonymousFunc descr (bif o: O(n))
+func FibonacciAnonymousFunc() func() int {
+	fmt.Println("--== Fibonacci anonymous func using ==--")
+	n1, n2 := 0, 1
+	return func() int {
+		n1, n2 = n2, n1+n2
+		return n1
+	}
+}
+
+// FibonacciGoldenRatio descr
+// Thanks wiki (Binet's Formula):
+// 		phi = (1+sqrt(5))/2 =~ 1.618033988749895
+//      formula: n = (pow(phi,n) - pow((1 - phi), n)) / sqrt(5)
+// negative numbers will be catched by uint
+func FibonacciGoldenRatio(n uint64) float64 {
+	fmt.Println("--== Fibonacci golden ratio ==--")
+	// const phi = 1.618033988749895
+	if n <= 1 {
+		return float64(n)
+	}
+	n = n - 1
+	phi := (1 + math.Pow(5, .5)) / 2.0
+	return math.Round((math.Pow(phi, float64(n)) - math.Pow((1-phi), float64(n))) / math.Sqrt(float64(5)))
+
 }
--- 01-go-level-1/fibonacci/main.go
+++ 01-go-level-1/fibonacci/main.go
@@ -1,10 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/evgeniyv6/gb_go_lvl1/01-go-level-1/fibonacci/fiblib"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
 )
 
+type intMap map[int]int
+
+func (m intMap) mapSorter() (sorted []int) {
+	for k, _ := range m {
+		sorted = append(sorted, k)
+	}
+	sort.Ints(sorted)
+	return
+}
+
+// Fibonacci printer
+// u need to point non-negative number, then one of the Fibonacci algorithms will be randomly selected:
+//   - using map
+//   - using golden ratio
+//   - anonymous func
 func main() {
-	fmt.Println("This is fibonacci main.go")
-	FibonacciMapUse()
+	const helper = "to use: print not negative number"
+	var num string
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Введите число > ")
+	for scanner.Scan() {
+		num = scanner.Text()
+		if scanner.Err() != nil {
+			fmt.Println("Error while stdin read.")
+		}
+		intNum, err := strconv.Atoi(num)
+		if err != nil || intNum < 0 {
+			fmt.Println("Cannot convert to int or number is negative.")
+			fmt.Printf("num %v\n", intNum)
+			fmt.Print("> ")
+		} else {
+			randChoiceNum := rand.Intn(3)
+			switch {
+			case randChoiceNum == 0:
+				nn := fiblib.FibonacciMapUse(intNum)
+				fmt.Print("Fibonacci numbers from algorithm (countdown starts from 0): ")
+				for _, k := range intMap(nn).mapSorter() {
+					fmt.Printf("%d ", nn[k])
+				}
+				fmt.Printf("\nFibonacci number for %d is: %d", intNum, nn[intNum-1])
+				fmt.Println()
+			case randChoiceNum == 1:
+				fib := fiblib.FibonacciAnonymousFunc()
+				fmt.Print("Fibonacci numbers from algorithm: ")
+				if intNum == 1 {
+					fmt.Print("0")
+				}
+				var res int
+				for i := 1; i < intNum; i++ {
+					res = fib()
+					fmt.Printf("%d ", res)
+				}
+				fmt.Printf("\nFibonacci number for %d is: %d", intNum, res)
+				fmt.Println()
+			case randChoiceNum == 2:
+				fib := fiblib.FibonacciGoldenRatio(uint64(intNum))
+				fmt.Printf("Fibonacci number for %d is: %v", intNum, fib)
+				fmt.Println()
+			default:
+				fmt.Println(helper)
+			}
+			fmt.Print("> ")
+		}
+	}
 }
